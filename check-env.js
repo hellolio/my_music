260023705d2413aa5dcad731945547cb312a0433
hellolio <helloliokin.outlook.com>
2025-04-17 23:19:10 +0800

    删除不必要的包

--- check-env.js
+++
@@ -1,2 +0,0 @@
-console.log('LIBCLANG_PATH =', process.env.LIBCLANG_PATH);
-console.log('PATH =', process.env.PATH);
--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,37 @@
+use std::{
+    env,
+    fs,
+    path::PathBuf,
+};
+
 fn main() {
+    // 要拷贝的 DLL 列表
+    let files = [
+        "avcodec-61.dll",
+        "avdevice-61.dll",
+        "avfilter-10.dll",
+        "avformat-61.dll",
+        "avutil-59.dll",
+        "swresample-5.dll",
+        "swscale-8.dll",
+    ];
+    let now_path= env::current_dir().unwrap();
+
+    // 获取 target/debug 目录（从 OUT_DIR 向上3级）
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let target_dir = out_dir
+        .ancestors()
+        .nth(3)
+        .expect("Failed to find target/debug")
+        .to_path_buf();
+
+    for file in files.iter() {
+        let src = now_path.join("bin").join(file);
+        let dest = target_dir.join(file);
+
+        println!("cargo:warning=Copying {:?} to {:?}", src, dest);
+        fs::copy(src, &dest).expect(&format!("Failed to copy {:?}", file));
+    }
+
     tauri_build::build()
 }
