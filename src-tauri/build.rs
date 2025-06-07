e2b3bf1b8f328369458a75a049358d081f0f2c40
hellolio <helloliokin@outlook.com>
2025-06-07 15:32:00 +0900

    ci

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,14 @@
 fn main() {
-    tauri_build::build()
+    if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=uuid");
+        println!("cargo:rustc-link-lib=ole32");
+        println!("cargo:rustc-link-lib=oleaut32");
+        println!("cargo:rustc-link-lib=strmiids");
+
+        println!("cargo:rustc-link-lib=mf");
+        println!("cargo:rustc-link-lib=mfplat");
+        println!("cargo:rustc-link-lib=mfreadwrite");
+        println!("cargo:rustc-link-lib=mfuuid");
+    }
+    tauri_build::build();
 }
