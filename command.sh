8002120b31ee7f09639765f6872fba37f0523c01
hellolio <helloliokin@outlook.com>
2026-01-24 21:53:05 +0900

    代码整理

--- command.sh
+++ command.sh
@@ -2,8 +2,3 @@ cd my-music
 npm install
 npm run tauri dev
 npm run tauri build
-
-# ctrl+ shift+i打开调试控制台
-# export FFMPEG_DIR=../vcpkg//installed//x64-windows
-# export VCPKG_ROOT=../vcpkg
-# export LIBCLANG_PATH="C://Program Files//LLVM//bin"
